d434df5e0dbbb68eae2d6c8aa53574f85b394d24
azharansari1996 <azharansari0206@gmail.com>
2022-10-18 20:15:37 +0530

    backend code

---
+++ app/model/MealTypeModel.js
@@ -0,0 +1,24 @@
+// import mongoose schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// creat schema
+
+const mealTypeSchema = new Schema({
+    name: {type:String},
+    content : {type:String},
+    image: {type:String},
+    meal_type :{type:Number},
+});
+
+
+// create model (collection)
+
+const mealTypeModel = mongoose.model('mealType', mealTypeSchema);
+
+
+// export model
+
+module.exports = mealTypeModel;
